185e6b290419d53a2ed04532043807d73d0efea8
atanuroy911 <atanuroy911@gmail.com>
2023-12-05 13:33:40 +0530

    gar

--- sage_gat.py
+++ sage_gat.py
@@ -34,20 +34,16 @@ from torch_geometric.nn import (
     MessagePassing,
 )
 
-from torch_geometric.data import NeighborSampler
-
-
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-class SAGE(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels, num_layers):
+class SAGEWithAttention(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
         super().__init__()
         self.convs = torch.nn.ModuleList()
-        for _ in range(num_layers):
-            self.convs.append(SAGEConv(in_channels, hidden_channels))
-            in_channels = hidden_channels
-        self.num_layers = num_layers
+        self.convs.append(GATConv(in_channels, hidden_channels, heads=1, dropout=0.2))
+        self.convs.append(SAGEConv(in_channels, hidden_channels))
+        self.convs.append(SAGEConv(hidden_channels, out_channels))
 
     def forward(self, x, edge_index):
         for i, conv in enumerate(self.convs):
@@ -55,6 +51,7 @@ class SAGE(torch.nn.Module):
             if i < len(self.convs) - 1:
                 x = x.relu_()
                 x = F.dropout(x, p=0.5, training=self.training)
+
         return x
 
     @torch.no_grad()
@@ -67,30 +64,14 @@ class SAGE(torch.nn.Module):
         # immediately computing the final representations of each batch:
         for i, conv in enumerate(self.convs):
             xs = []
-            for batch_size, n_id, adj in subgraph_loader:
-                x = x_all[n_id].to(device)
-                x = conv(x, adj.to(device))
+            for batch in subgraph_loader:
+                x = x_all[batch.n_id.to(x_all.device)].to(device)
+                x = conv(x, batch.edge_index.to(device))
                 if i < len(self.convs) - 1:
                     x = x.relu_()
-                xs.append(x.cpu())
-                pbar.update(batch_size)
+                xs.append(x[:batch.batch_size].cpu())
+                pbar.update(batch.batch_size)
             x_all = torch.cat(xs, dim=0)
+        
         pbar.close()
         return x_all
-
-class NodeSampler(torch.utils.data.DataLoader):
-    def __init__(self, data, size, num_layers):
-        super(NodeSampler, self).__init__(data, batch_size=size, shuffle=True)
-
-        self.size = size
-        self.num_layers = num_layers
-
-    def collate_fn(self, data_list):
-        batch_size = len(data_list)
-        n_id = torch.cat([data.n_id for data in data_list], dim=0)
-        adj = NeighborSampler(data_list, size=self.size, num_hops=self.num_layers, batch_size=batch_size,
-                              shuffle=False).adj_t
-        return batch_size, n_id, adj
-
-    
-
